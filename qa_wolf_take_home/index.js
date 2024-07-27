e5ab1f69170c4e4b17134a7dc33f709a5080bc7e
kschieman <kevin.schieman@outlook.com>
2024-07-27 02:24:53 -0400

    Added a check for a 200 response from Hacker News

--- qa_wolf_take_home/index.js
+++ qa_wolf_take_home/index.js
@@ -5,7 +5,7 @@ async function sortHackerNewsArticles() {
   const context = await browser.newContext();
   const page = await context.newPage();
 
-  // Create an Article class with all needed information
+  // - create an Article class with all needed information
   class Article {
     constructor(age, title, link) {
       this.Age = age;
@@ -20,24 +20,32 @@ async function sortHackerNewsArticles() {
       if (await nextButton.isVisible()) {
         console.log("Clicking 'More' button");
         await nextButton.click();
-        // Wait for a new set of articles to appear by waiting for the first title in the new list
+        // - wait for a new set of articles to appear by waiting for the first title in the new list
         await page.waitForSelector('.titleline a');
       } else {
         console.error('Next button not found');
       }
     } catch (error) {
-      console.error('Error in clickNextButton:', error);
+      console.error('Error in clickNextButton: ', error);
     }
   }
 
-  await page.goto("https://news.ycombinator.com/newest");
-  console.log("Navigated to Hacker News");
+  const request = await page.goto("https://news.ycombinator.com/newest");
+
+  if (request.status() === 200) {
+    console.log("Successfully navigated to Hacker News");
+  } else {
+    console.log("Unable to navigate to page. Status code:", request.status() + `
+    - Please check connection.`);
+    return;
+  }
+  
 
   let fetchedArticles = [];
 
   while (fetchedArticles.length < 100) {
     try {
-      // Fetch titles and links
+      // - fetch titles and links
       let titles = await page.locator('.titleline');
       let titleContents = await titles.allTextContents();
       let links = await titles.evaluateAll(anchors => anchors.map(anchor => anchor.querySelector('a').href));
@@ -49,32 +57,34 @@ async function sortHackerNewsArticles() {
           titleContents[j],
           links[j]
         );
-
+        // - push article into list
         fetchedArticles.push(article);
         if (fetchedArticles.length >= 100) {
+          // - once we have found 100 articles the loop will break
           break;
         }
       }
 
       if (fetchedArticles.length < 100) {
+        // - only go to next page until we have 100 articles 
         await clickNextButton();
       }
 
       if (fetchedArticles.length >= 100) {
-        console.log("Fetched articles: ", fetchedArticles.length + " - Closing Browser");
+        console.log("Collected ", fetchedArticles.length + " articles - Closing Browser");
         await browser.close();
         break;
       }
     } catch (error) {
-      console.error('Error in while loop:', error);
+      console.error('Error in while loop: ', error);
       break;
     }
   }
 
   fetchedArticles = fetchedArticles.slice(0, 100);
-  console.log("Fetched articles:", fetchedArticles);
+  console.log("Fetched articles: ", fetchedArticles);
 
-  // Verify all ages are newest to oldest
+  // - verify all ages are newest to oldest
   function convertToMinutes(ageString) {
     const [value, unit] = ageString.split(' ');
     const number = parseInt(value, 10);
@@ -93,13 +103,14 @@ async function sortHackerNewsArticles() {
     const nextMinutes = convertToMinutes(fetchedArticles[i + 1].Age);
 
     if (currentMinutes > nextMinutes) {
-      console.error("Articles are not ordered from newest to oldest");
+      console.error("Articles are not ordered from newest to oldest, retrying");
+      await sortHackerNewsArticles();
       return;
+    } else {
+        console.log("All " + fetchedArticles.length + " articles from Hacker News are ordered from newest to oldest");
+        return fetchedArticles;
     }
   }
-
-  console.log("All " + fetchedArticles.length + " articles from Hacker News are ordered from newest to oldest");
-  return fetchedArticles;
 }
 
 (async () => {
